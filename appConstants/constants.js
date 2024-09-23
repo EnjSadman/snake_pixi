2a96294592a77ccf04cf8f658396c338f889c507
roman <roman.pavlenko.job@gmail.com>
2024-09-23 11:30:22 +0300

    first commit

---
+++ appConstants/constants.js
@@ -0,0 +1,9 @@
+export const APP_CONSTANTS = {
+  WIDTH: 1000,
+  HEIGHT: 600,
+  BASE_TILE_WIDTH: 20,
+  GAME_HEIGHT: 20,
+  GAME_WIDTH: 20,
+  SNAKE_WIDTH: 3,
+
+}
---
+++ main.js
@@ -0,0 +1,36 @@
+import { Application, Graphics } from "pixi.js";
+import { APP_CONSTANTS } from "./appConstants/constants";
+import { describeBorders } from "./lib/describeBorders";
+
+
+(async() => {
+  const app = new Application();
+
+  await app.init({
+    width: APP_CONSTANTS.WIDTH,
+    height: APP_CONSTANTS.HEIGHT
+  });
+
+  const borders = describeBorders(APP_CONSTANTS.GAME_WIDTH, APP_CONSTANTS.GAME_HEIGHT, APP_CONSTANTS.BASE_TILE_WIDTH);
+
+  for (let i = 0; i < borders.length; i++) {
+    const rectangle = new Graphics()
+    .rect(
+      borders[i][0],
+      borders[i][1],
+      APP_CONSTANTS.BASE_TILE_WIDTH,
+      APP_CONSTANTS.BASE_TILE_WIDTH,
+    )
+    .fill({
+      color: 0x00000
+    })
+    .stroke({
+      width: 1,
+      color: 0xffffff
+    })
+
+    app.stage.addChild(rectangle);
+  }
+
+  document.body.appendChild(app.canvas);
+})();
